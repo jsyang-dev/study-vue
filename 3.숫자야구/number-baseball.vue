c000361e0afa5a6a8d328cacf0e113e1d6c55b65
Jeongsu Yang <mycat83@gmail.com>
2020-07-11 22:18:58 +0900

    3-2. 프로젝트 구조와 웹팩 빌드

---
+++ 3.숫자야구/number-baseball.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>{{result}}</h1>
+        <form v-on:submit="onSubmitForm">
+            <input ref="answer" maxlength="4" v-model="value"/>
+        </form>
+    </div>
+    <div>시도: {{}}</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                value: '',
+                result: ''
+            }
+        },
+        methods: {
+            onSubmitForm(e) {
+                e.preventDefault();
+            }
+        }
+    };
+</script>
+
+<style>
+
+</style>
