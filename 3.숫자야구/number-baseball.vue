851f61cd7df1739ad2768edba8ab4a5ea9f4189e
Jeongsu Yang <mycat83@gmail.com>
2020-07-13 22:34:18 +0900

    3-5. 숫자야구 완성하기

--- 3.숫자야구/number-baseball.vue
+++ 3.숫자야구/number-baseball.vue
@@ -16,9 +16,20 @@
 </template>
 
 <script>
+    const getNumbers = () => {
+        const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        const array = [];
+        for (let i = 0; i < 4; i += 1) {
+            const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+            array.push(chosen);
+        }
+        return array;
+    };
+
     export default {
         data() {
             return {
+                answer: getNumbers(),
                 tries: [],
                 value: '',
                 result: ''
@@ -26,10 +37,42 @@
         },
         methods: {
             onSubmitForm() {
-                this.tries.push({
-                    try: this.value,
-                    result: '홈런',
-                });
+                if (this.value === this.answer.join('')) {
+                    this.tries.push({
+                        try: this.value,
+                        result: '홈런',
+                    });
+                    this.result = '홈런';
+                    alert('게임을 다시 실행합니다.');
+                    this.value = '';
+                    this.answer = getNumbers();
+                    this.tries = [];
+                    this.$refs.answer.focus();
+                } else {
+                    if (this.tries.length >= 9) {
+                        this.result = `10번 넘게 틀려서 실패!! 답은 ${this.answer.join(',')}였습니다!`;
+                        alert('게임을 다시 시작합니다.');
+                        this.value = '';
+                        this.answer = getNumbers();
+                        this.tries = [];
+                        this.$refs.answer.focus();
+                    }
+                    let strike = 0;
+                    let ball = 0;
+                    const answerArray = this.value.split('').map(v => parseInt(v));
+                    for (let i = 0; i < 4; i += 1) {
+                        if (answerArray[i] === this.answer[i]) {
+                            strike++;
+                        } else if (this.answer.includes(answerArray[i])) {
+                            ball++;
+                        }
+                    }
+                    this.tries.push({
+                        try: this.value,
+                        result: `${strike} 스트라이크, ${ball} 볼입니다.`
+                    })
+                }
+
                 this.value = '';
                 this.$refs.answer.focus();
             }
